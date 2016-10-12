2b729ffd6e4965d046579d5e367035551211eb55
anton mikhaylov <g-man2006@yandex.ru>
2016-10-12 18:50:18 +0300

    pre-alpha

--- app.js
+++ app.js
@@ -336,8 +336,19 @@ function receivedPostback(event) {
     if (payload) {
         switch (payload) {
             case 'startConversationPayload':
+                sendQuickReplyTutorialChoice(senderID);
+                break;
+            case 'mainMenuPayload':
                 sendStartOptionsMessage(senderID);
                 break;
+            case 'tutorialPayload':
+                sendTutorialMessage(senderID);
+                break;
+            case 'quickReplyTutorialYes':
+                sendTextMessage(senderID, "Чуть позже тут появится обучение");
+                break;
+            case 'quickReplyTutorialNo':
+                sendStartOptionsMessage(senderID);
             case 'cardStatusPayload':
                 sendCardStatusMessage(senderID);
                 break;
@@ -419,92 +430,10 @@ function sendTextMessage(recipientId, messageText) {
     callSendAPI(messageData);
 }
 
-/*
- * Send a button message using the Send API.
- *
- */
-function sendButtonMessage(recipientId) {
-    var messageData = {
-        recipient: {
-            id: recipientId
-        },
-        message: {
-            attachment: {
-                type: "template",
-                payload: {
-                    template_type: "button",
-                    text: "This is test text",
-                    buttons: [{
-                        type: "web_url",
-                        url: "https://www.oculus.com/en-us/rift/",
-                        title: "Open Web URL",
-                        webview_height_ratio: "compact"
-                    }, {
-                        type: "postback",
-                        title: "Trigger Postback",
-                        payload: "DEVELOPED_DEFINED_PAYLOAD"
-                    }, {
-                        type: "phone_number",
-                        title: "Call Phone Number",
-                        payload: "+16505551234"
-                    }]
-                }
-            }
-        }
-    };
 
-    callSendAPI(messageData);
-}
 
-/*
- * Send a Structured Message (Generic Message type) using the Send API.
- *
- */
-function sendGenericMessage(recipientId) {
-    var messageData = {
-        recipient: {
-            id: recipientId
-        },
-        message: {
-            attachment: {
-                type: "template",
-                payload: {
-                    template_type: "generic",
-                    elements: [{
-                        title: "rift",
-                        subtitle: "Next-generation virtual reality",
-                        item_url: "https://www.oculus.com/en-us/rift/",
-                        image_url: SERVER_URL + "/assets/rift.png",
-                        buttons: [{
-                            type: "web_url",
-                            url: "https://www.oculus.com/en-us/rift/",
-                            title: "Open Web URL"
-                        }, {
-                            type: "postback",
-                            title: "Call Postback",
-                            payload: "Payload for first bubble",
-                        }],
-                    }, {
-                        title: "touch",
-                        subtitle: "Your Hands, Now in VR",
-                        item_url: "https://www.oculus.com/en-us/touch/",
-                        image_url: SERVER_URL + "/assets/touch.png",
-                        buttons: [{
-                            type: "web_url",
-                            url: "https://www.oculus.com/en-us/touch/",
-                            title: "Open Web URL"
-                        }, {
-                            type: "postback",
-                            title: "Call Postback",
-                            payload: "Payload for second bubble",
-                        }]
-                    }]
-                }
-            }
-        }
-    };
-
-    callSendAPI(messageData);
+function sendTutorialMessage(recipientId) {
+    sendQuickReplyTutorialChoice(recipientId);
 }
 
 function sendStartOptionsMessage(recipientId) {
@@ -645,99 +574,29 @@ function sendAccountsInfoMessage(recipientId) {
 }
 
 
-/*
- * Send a receipt message using the Send API.
- *
- */
-function sendReceiptMessage(recipientId) {
-    // Generate a random receipt ID as the API requires a unique ID
-    var receiptId = "order" + Math.floor(Math.random() * 1000);
-
-    var messageData = {
-        recipient: {
-            id: recipientId
-        },
-        message: {
-            attachment: {
-                type: "template",
-                payload: {
-                    template_type: "receipt",
-                    recipient_name: "Peter Chang",
-                    order_number: receiptId,
-                    currency: "USD",
-                    payment_method: "Visa 1234",
-                    timestamp: "1428444852",
-                    elements: [{
-                        title: "Oculus Rift",
-                        subtitle: "Includes: headset, sensor, remote",
-                        quantity: 1,
-                        price: 599.00,
-                        currency: "USD",
-                        image_url: SERVER_URL + "/assets/riftsq.png"
-                    }, {
-                        title: "Samsung Gear VR",
-                        subtitle: "Frost White",
-                        quantity: 1,
-                        price: 99.99,
-                        currency: "USD",
-                        image_url: SERVER_URL + "/assets/gearvrsq.png"
-                    }],
-                    address: {
-                        street_1: "1 Hacker Way",
-                        street_2: "",
-                        city: "Menlo Park",
-                        postal_code: "94025",
-                        state: "CA",
-                        country: "US"
-                    },
-                    summary: {
-                        subtotal: 698.99,
-                        shipping_cost: 20.00,
-                        total_tax: 57.67,
-                        total_cost: 626.66
-                    },
-                    adjustments: [{
-                        name: "New Customer Discount",
-                        amount: -50
-                    }, {
-                        name: "$100 Off Coupon",
-                        amount: -100
-                    }]
-                }
-            }
-        }
-    };
-
-    callSendAPI(messageData);
-}
 
 /*
  * Send a message with Quick Reply buttons.
  *
  */
-function sendQuickReply(recipientId) {
+function sendQuickReplyTutorialChoice(recipientId) {
     var messageData = {
         recipient: {
             id: recipientId
         },
         message: {
-            text: "What's your favorite movie genre?",
-            metadata: "DEVELOPER_DEFINED_METADATA",
+            text: "Хочешь, я немного расскажу о себе?",
+            metadata: "quickReplyTutorialChoice-meta",
             quick_replies: [
                 {
                     "content_type": "text",
-                    "title": "Action",
-                    "payload": "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_ACTION"
+                    "title": "Да, расскажи, что ты умеешь",
+                    "payload": "quickReplyTutorialYes"
                 },
                 {
                     "content_type": "text",
-                    "title": "Comedy",
-                    "payload": "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_COMEDY"
-                },
-                {
-                    "content_type": "text",
-                    "title": "Drama",
-                    "payload": "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_DRAMA"
+                    "title": "Нет, я всё знаю",
+                    "payload": "quickReplyTutorialNo"
                 }
             ]
         }
