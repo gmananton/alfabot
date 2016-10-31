1e14d48ade91b0a754ae3aecf6119de2e0546e03
Sergey Tyapkin <styapkin@alfabank.ru>
2016-10-31 18:35:07 +0300

    d

--- app.js
+++ app.js
@@ -810,8 +810,8 @@ function callSendAPI_test(messageData) {
     request({
         uri: 'https://graph.facebook.com/v2.6/me/messages',
        // qs: {access_token: PAGE_ACCESS_TOKEN},
-        qs: {access_token: "EAAQsBloQSawBAPYjfCCWYi2G2XqcdCZAi30xPP6GowrREl8sfHSJZBwD8ILI10r0Bq4PIHZCZA6ewZCjqV8ZAB3LKH2OaUD2y53uTutVUmlKMeYhNwF8sdZA5AgEe0uS4LUCfMnk9JY7X2EWOFnS7FVTJyMgtAyP7E0h0YvudwJJgZDZD"},
-        //qs: {access_token: "EAAEajzOnTK8BAPtuAKWJ767fZBPj1hclknhxCpaZApkKSi5cO7H5JZCciiVXv3BvobknwPRBIbKvRxveOU2A4m8Xqtc4ZAZA89d2U7Gn88NOoMqXjXrWzZByLplIaDyoCIGrXyhdBKMQS4r7aYo5klQnOP4yeWiFZB3rZCZAZBw6DUuAZDZD"},
+       // qs: {access_token: "EAAQsBloQSawBAPYjfCCWYi2G2XqcdCZAi30xPP6GowrREl8sfHSJZBwD8ILI10r0Bq4PIHZCZA6ewZCjqV8ZAB3LKH2OaUD2y53uTutVUmlKMeYhNwF8sdZA5AgEe0uS4LUCfMnk9JY7X2EWOFnS7FVTJyMgtAyP7E0h0YvudwJJgZDZD"},
+        qs: {access_token: "EAAEajzOnTK8BAPtuAKWJ767fZBPj1hclknhxCpaZApkKSi5cO7H5JZCciiVXv3BvobknwPRBIbKvRxveOU2A4m8Xqtc4ZAZA89d2U7Gn88NOoMqXjXrWzZByLplIaDyoCIGrXyhdBKMQS4r7aYo5klQnOP4yeWiFZB3rZCZAZBw6DUuAZDZD"},
 
         method: 'POST',
         json: messageData
